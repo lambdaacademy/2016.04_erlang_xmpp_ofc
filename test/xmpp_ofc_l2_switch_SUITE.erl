593d033e28b6d8eefe2c967f8a1280fbb586bb3c
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2016-05-12 12:52:03 +0200

    Add stub for another CT test

--- test/xmpp_ofc_l2_switch_SUITE.erl
+++ test/xmpp_ofc_l2_switch_SUITE.erl
@@ -22,7 +22,8 @@ end_per_testcase(_TestCase, Config) ->
     ok = xmpp_ofc_l2_switch:stop(?config(pid, Config)).
 
 all() ->
-    [l2_mod_sends_packet_out].
+    [l2_mod_sends_packet_out,
+     l2_mod_sends_flow_mod].
 
 
 %%--------------------------------------------------------------------
@@ -40,3 +41,27 @@ l2_mod_sends_packet_out(Config) ->
               fun(#ofp_message{body = #ofp_packet_out{}}) -> true;
                  (_) -> false
               end, Response)).
+
+l2_mod_sends_flow_mod(Config) ->
+    %% The flow mod is sent only if the dst mac to port mapping is known
+    %%
+    %% To test it we can send PacketIn1; then PacketIn2 that
+    %% PacketIn2.DstMac == PacketIn.SrcMac as the l2 mod lerns src macs
+    {ok, [PacketIn1]} =
+        file:consult(filename:join([?config(data_dir, Config),
+                                    "packet_in.data"])),
+    PacketIn2 = set_packet_in_dst_mac(PacketIn1,
+                                      get_packet_in_src_mac(PacketIn1)),
+    _Response1 = xmpp_ofc_l2_switch:handle_message(?config(pid, Config),
+                                                   PacketIn1,
+                                                   []),
+    Response2 = xmpp_ofc_l2_switch:handle_message(?config(pid, Config),
+                                                 PacketIn2,
+                                                 []),
+    ?assert(lists:any(
+              fun(#ofp_message{body = #ofp_flow_mod{}}) -> true;
+                 (_) -> false
+              end, Response2)).
+
+set_packet_in_dst_mac(_, _) -> ok.
+get_packet_in_src_mac(_) -> ok.
